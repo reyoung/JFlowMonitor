5fcac50d49b4997ee688779cc966355e49c8808c
reyoung <reyoung@126.com>
2011-04-29 00:00:47 +0800

    Add PcapConsole Test

---
+++ selftest/reyoung/PcapConsole/src/pcapconsole/Main.java
@@ -0,0 +1,21 @@
+/*
+ * This file is written by reyoung, reyoung@126.com.
+ */
+
+package pcapconsole;
+
+/**
+ *
+ * @author Reyoung
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("Hello world\n");
+    }
+
+}
