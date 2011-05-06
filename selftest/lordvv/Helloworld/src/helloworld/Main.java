b58b4e898dfae4729281ce8547f8dcf1a4eeac68
XuDaWei <lordvv@xnmsn.cn>
2011-05-06 19:00:02 +0800

    ......

--- selftest/lordvv/Helloworld/src/helloworld/Main.java
+++ selftest/lordvv/Helloworld/src/helloworld/Main.java
@@ -4,20 +4,11 @@
  */
 
 package helloworld;
-import org.jfree.JCommon;
+
 /**
  *
  * @author Administrator
  */
 public class Main {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Hello World!");
-
-    }
 
 }
