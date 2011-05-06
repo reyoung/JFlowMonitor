25b29c04eb066808e131a022d1d348254f74f98e
reyoung <reyoung@126.com>
2011-05-06 22:45:46 +0800

    Add HttpFilter TcpFilter

---
+++ JFlowMonitor/src/Logic/Filters/TcpFilter.java
@@ -0,0 +1,29 @@
+/*
+ * This file is written by reyoung, reyoung@126.com.
+ */
+
+package Logic.Filters;
+
+import Network.IPacket;
+
+/**
+ *
+ * @author Reyoung
+ */
+public class TcpFilter implements IPacketFilter{
+    private TcpFilter(){
+
+    }
+    private static TcpFilter instance = null;
+    public TcpFilter Instance(){
+        if(instance == null){
+            instance = new TcpFilter();
+        }
+        return instance;
+    }
+
+    public boolean check(IPacket packet) {
+        return (packet.getPacketFlag()&IPacket.PacketFlag_TCP)!=0;
+    }
+    
+}
