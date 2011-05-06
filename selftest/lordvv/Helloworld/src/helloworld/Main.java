b37953e8f72f03c54d53b2424da2876c8f675d60
XuDaWei <lordvv@xnmsn.cn>
2011-05-06 15:12:15 +0800

    Add JFreeChart to 3rdparty, Start JFreeChart tests.

---
+++ selftest/lordvv/Helloworld/src/helloworld/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package helloworld;
+import org.jfree.JCommon;
+/**
+ *
+ * @author Administrator
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World!");
+
+    }
+
+}
