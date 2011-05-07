006f4c9d273f432b7bd0efad9a50ab25c5114b26
reyoung <reyoung@126.com>
2011-05-07 13:23:49 +0800

    Start SmallPanelTest

---
+++ selftest/reyoung/SmallPanelTest/src/smallpaneltest/Main.java
@@ -0,0 +1,20 @@
+/*
+ * This file is written by reyoung, reyoung@126.com.
+ */
+
+package smallpaneltest;
+
+/**
+ *
+ * @author Reyoung
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
