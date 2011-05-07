1643c69fbefa6d29839ccfad24e1bbb211dff33b
reyoung <reyoung@126.com>
2011-05-07 14:05:23 +0800

    Add SmallPanelTest

--- selftest/reyoung/SmallPanelTest/src/smallpaneltest/Main.java
+++ selftest/reyoung/SmallPanelTest/src/smallpaneltest/Main.java
@@ -4,6 +4,8 @@
 
 package smallpaneltest;
 
+import java.awt.Panel;
+
 /**
  *
  * @author Reyoung
@@ -14,7 +16,12 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        final SmallPanel panel = new SmallPanel();
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                panel.setVisible(true);
+            }
+        });
     }
 
 }
