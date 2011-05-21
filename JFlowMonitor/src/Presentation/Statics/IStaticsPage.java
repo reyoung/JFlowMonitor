67710671759165b51046872ce69c9fa0cb298781
reyoung <reyoung@126.com>
2011-05-21 13:34:55 +0800

    Static Window Complete.

---
+++ JFlowMonitor/src/Presentation/Statics/IStaticsPage.java
@@ -0,0 +1,20 @@
+/*
+ * This file is written by reyoung, reyoung@126.com.
+ */
+
+package Presentation.Statics;
+
+import java.awt.Component;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Reyoung
+ */
+public interface IStaticsPage {
+    @Override
+    public String toString();
+    
+    public Component getWidget();
+    
+}
