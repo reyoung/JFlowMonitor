c346cfc934f24460d9bb222e30a2af11507a968b
XuDaWei <lordvv@xnmsn.cn>
2011-05-26 10:46:49 +0800

    Visits descending

--- JFlowMonitor/src/Presentation/Statics/MostVisitedPage.java
+++ JFlowMonitor/src/Presentation/Statics/MostVisitedPage.java
@@ -2,6 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package Presentation.Statics;
 
 import java.awt.Component;
@@ -10,18 +11,16 @@ import java.awt.Component;
  *
  * @author Administrator
  */
-public class AnalysisChartPage extends AbstractStaticsPage {
+public class MostVisitedPage extends AbstractStaticsPage{
 
     @Override
     protected String doToString() {
-        return "Flow of Sites";
+        return "Most Visited Site";
     }
 
     @Override
     protected Component doGetWidget() {
-
-       
-        return new TimeSelectPanel();
+        return new MostVisitedPanel();
     }
 
 }
